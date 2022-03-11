f3bbb07bfdcab1dfc13803e6d66e83913519165b
Patrick Viscount <71523753+patrickviscount@users.noreply.github.com>
2022-03-11 14:04:58 -0400

    Send document in through props

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,37 +2,21 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import MaterialTable from "material-table";
 import { Checkbox, Select, MenuItem } from "@material-ui/core";
-import Modal from "./Modal"
-const URL = "http://localhost:4000/DataStreams";
-let oldSeg = "all";
+import Modal from "./Modal";
 var checker = 1;
 
 function DataTable() {
-
   const [data, setData] = useState([]);
   const [filter, setFilter] = useState(false);
   const [view, setView] = useState(true);
   const [segment, setSegment] = useState("all");
   const [show, setShow] = useState(false);
   const [idNum, setIdNum] = useState(1);
+  const [file, setFile] = useState("");
 
-  useEffect(()=>{
-    // getDataList();
-    // let x = document.querySelectorAll(".MuiIconButton-root");
-    // console.log(x);
-    // x[6].addEventListener('click', function() {
-    // if (checker === 1)
-    //   checker = 0;
-    // else
-    //   checker = 1;
-    // handleColumns();
-    // });
-  },[]);
-
-  // const getDataList=()=> {
-  //   fetch(URL).then(resp=>resp.json())
-  //   .then(resp=>setData(resp))
-  // };
+  useEffect(() => {
+    setFile("../../test.pdf");
+  }, []);
 
   const getColumns = () => {
     if (checker === 1) {
@@ -45,34 +29,67 @@ function DataTable() {
       columns[11].hidden = true;
     }
     return columns;
-  }
+  };
 
   const columns = [
-    { title: "ID", field: "id", editable: false},
-    { title: "Vendor name", field: "Vendor name"},
+    { title: "ID", field: "id", editable: false },
+    { title: "Vendor name", field: "Vendor name" },
     {
       title: "More Info",
       render: (rowData) => (
         <button onClick={() => handleClickModal(rowData["id"])}>
-          More Info </button> 
-          )},
-    { title: "Vendor contact", field: "Vendor contact"},
-    { title: "Buisness Unit Acquiring", field: "Buisness Unit Acquiring"},
+          More Info{" "}
+        </button>
+      )
+    },
+    { title: "Vendor contact", field: "Vendor contact" },
+    { title: "Buisness Unit Acquiring", field: "Buisness Unit Acquiring" },
     {
       title: "Lead Data Steward",
       field: "Lead Data Steward",
-      render: (rowData) => <div> {rowData["Lead Data Steward"]} |<a href={`${findURL(rowData)}`} target="_blank" rel="noopener noreferrer"> Contact </a> </div>
+      render: (rowData) => (
+        <div>
+          {" "}
+          {rowData["Lead Data Steward"]} |
+          <a
+            href={`${findURL(rowData)}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            Contact{" "}
+          </a>{" "}
+        </div>
+      )
     },
     {
       title: "Business Contact",
       field: "Business Contact",
-      render: (rowData) => <div> {rowData["Business Contact"]} |<a href={`${findURL(rowData)}`} target="_blank" rel="noopener noreferrer"> Contact </a> </div>
+      render: (rowData) => (
+        <div>
+          {" "}
+          {rowData["Business Contact"]} |
+          <a
+            href={`${findURL(rowData)}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            Contact{" "}
+          </a>{" "}
+        </div>
+      )
     },
     { title: "Main Users of Data", field: "Main Users of Data" },
     { title: "Brief Desc of Data Used", field: "Brief Desc of Data Used" },
     { title: "Value Dervied From Data", field: "Value Dervied From Data" },
     { title: "Contracts in Zycus", field: "Contracts in Zycus" },
-    { title: "IT Source", field: "IT Source", hidden: true, hiddenByColumnsButton: true}
+    {
+      title: "IT Source",
+      field: "IT Source",
+      hidden: true,
+      hiddenByColumnsButton: true
+    }
   ];
 
   function handleClickModal(Id) {
@@ -82,26 +99,26 @@ function DataTable() {
 
   const handleColumns = () => {
     setView(!view);
-    if(checker === 0){
+    if (checker === 0) {
       checker = 1;
-    }
-    else {
+    } else {
       checker = 0;
     }
     getColumns();
-  }
+  };
 
   const handleCheck = () => {
     setFilter(!filter);
-  }
+  };
 
   return (
     <div className="App">
       <h1 align="center">DataTable using the Material Table library</h1>
-      <h4 align="center">
-        Please see the react component below
-      </h4>
-      <button onClick={()=>handleClickModal(1)}> open react document viewer popup</button>
+      <h4 align="center">Please see the react component below</h4>
+      <button onClick={() => handleClickModal(1)}>
+        {" "}
+        open react document viewer popup
+      </button>
       <MaterialTable
         title="3rd Party Vendors"
         data={Array.from(data)}
@@ -114,56 +131,54 @@ function DataTable() {
           draggable: true,
           paginationType: "stepped"
         }}
-
-      actions = {[
-        {
-          icon:() => 
-          <Checkbox
-          checked = {view}
-          onChange={handleColumns}
-          inputProps={{'aria-label': 'primary checkbox'}}
-          color="primary"
-          />,
-          tooltip:"Hide/Show Columns",
-          isFreeAction: true
-        },
-        {
-        icon:() => 
-        <Checkbox
-        checked = {filter}
-        onChange={handleCheck}
-        inputProps={{'aria-label': 'primary checkbox'}}
-        color="primary"
-        />,
-        tooltip:"Hide/Show Filter",
-        isFreeAction: true
-        },
-        {
-          icon: () => 
-          <Select
-          labelId="demo-simple-select-label"
-          id="demo-select-simple"
-          style={{width: 100}}
-          value={segment}
-          onChange={(e) => {
-            setSegment(e.target.value);
-          }}
-          >
-          <MenuItem value={"all"}> All </MenuItem>
-          <MenuItem value={"Insurance"}> Insurance </MenuItem>
-          <MenuItem value={"Re-insurance"}> Re-insurance </MenuItem>
-          <MenuItem value={"Admin"}> Admin </MenuItem>
-          </Select>,
-          tooltip: "Filter Users of Data",
-          isFreeAction: true
-        }
-      ]}
+        actions={[
+          {
+            icon: () => (
+              <Checkbox
+                checked={view}
+                onChange={handleColumns}
+                inputProps={{ "aria-label": "primary checkbox" }}
+                color="primary"
+              />
+            ),
+            tooltip: "Hide/Show Columns",
+            isFreeAction: true
+          },
+          {
+            icon: () => (
+              <Checkbox
+                checked={filter}
+                onChange={handleCheck}
+                inputProps={{ "aria-label": "primary checkbox" }}
+                color="primary"
+              />
+            ),
+            tooltip: "Hide/Show Filter",
+            isFreeAction: true
+          },
+          {
+            icon: () => (
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-select-simple"
+                style={{ width: 100 }}
+                value={segment}
+                onChange={(e) => {
+                  setSegment(e.target.value);
+                }}
+              >
+                <MenuItem value={"all"}> All </MenuItem>
+                <MenuItem value={"Insurance"}> Insurance </MenuItem>
+                <MenuItem value={"Re-insurance"}> Re-insurance </MenuItem>
+                <MenuItem value={"Admin"}> Admin </MenuItem>
+              </Select>
+            ),
+            tooltip: "Filter Users of Data",
+            isFreeAction: true
+          }
+        ]}
       />
-      <Modal
-        show={show}
-        idNumber={idNum}
-        onClose={() => setShow(false)}
-      />
+      <Modal show={show} file={file} onClose={() => setShow(false)} />
     </div>
   );
 }
@@ -182,24 +197,22 @@ function findID(dataStream) {
 
 function findURL(dataStream) {
   if (dataStream.WorkdayLink) {
-  let data = dataStream.WorkdayLink.split(" ");
-  let url = "";
-  
-  for (let i = 0; i < data.length; i++) {
-    if( data[i].startsWith("https://www.myworkday.com")){
-      url = data[i];
-      return url;
+    let data = dataStream.WorkdayLink.split(" ");
+    let url = "";
+
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].startsWith("https://www.myworkday.com")) {
+        url = data[i];
+        return url;
+      } else if (data[i].startsWith("mailtdo")) {
+        url = data[i];
+      }
     }
-    else if (data[i].startsWith("mailtdo")) {
-      url = data[i];
+    if (url === "") {
+      url = "https://www.axiscapital.com/";
     }
-  }
-  if (url === "") {
-    url = "https://www.axiscapital.com/";
-  }
-  return url;
-  }
-  else {
+    return url;
+  } else {
     return "https://www.axiscapital.com/";
   }
 }
--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,104 +1,96 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import "./Modal.css";
 import DocViewer, { DocViewerRenderers } from "react-doc-viewer";
 
 const Modal = (props) => {
-    const URL = "http://localhost:4000/DataStreams";
-    let ID = props.idNumber;
+  const [loading, setLoading] = useState(false);
 
-    const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(true);
-  
-    // function getDataList() {
-    //   fetch(`${URL}/${ID}`).then(resp=>resp.json())
-    //   .then(resp=>setData(resp));
-    //     setLoading(false);
-    // }
+  var docs = [];
 
-    const closeOnEsc = (e) => {
-        if((e.charCode || e.keyCode) === 27){
-            props.onClose();
-        }
-    }
-
-    useEffect(() => {
-        setLoading(true);
-        // getDataList();
-        document.body.addEventListener('keydown', closeOnEsc);
-        return function cleanup() {
-            document.body.removeEventListener('keydown', closeOnEsc);
-        }
-    }, [])
-
-    useEffect(() => {
-        // getDataList();
-    }, [props])
-
-    if(!props.show){
-        return null;
-    }
-    if(loading === true){
-      return (<div className='modal'> <div className='modal-content'><h1>Content isn't finished loading</h1> </div> </div>)
-    }
-
-    return (
-        <div className='modal' onClick={props.onClose} style={{height: document.body.scrollHeight+25}}>
-            <div className='modal-content' onClick={e => e.stopPropagation()}>
-                <div className='modal-header'>
-                    <h4 className='modal-title'>{data["Vendor name"]}</h4>
-                </div>
-                <div className='modal-body'>
-                <h6> Vendor contact: </h6> {data["Vendor contact"]} <br /> <hr />
-                <h6> Buisness Unit Acquiring:</h6> {data["Buisness Unit Acquiring"]} <br /> <hr />
-                <h6> Lead Data Steward:</h6> {data["Lead Data Steward"]} <br /> <hr />
-                <h6> Business Contact:</h6> {data["Business Contact"]} <br /> <hr />
-                <h6> Main Users of Data:</h6> {data["Main Users of Data"]} <br /> <hr />
-                <h6> Brief Desc of Data Used:</h6> {data["Brief Desc of Data Used"]} <br /> <hr />
-                <h6> Value Dervied From Data:</h6> {data["Value Dervied From Data"]} <br /> <hr />
-                <h6> Contracts in Zycus:</h6> {data["Contracts in Zycus"]} <br /> <hr />
-                <h6> IT Source:</h6> {data["IT Source"]} <br /> <hr />
-                 <div>
-                <h6>Contact at Axis:</h6>
-                <a
-                    href={`${findURL(data)}`}
-                    target="_blank"
-                    rel="noopener noreferrer" 
-                    >
-                    {findURL(data)}
-                </a>
-                </div> <br /> <hr />
-                </div>
-                <div className='modal-footer'>
-                    <button onClick={props.onClose} className='exitButton'>X</button>
-                </div>
-            </div>
-        </div>
-
-    )
-}
-
-function findURL(dataStream) {
-    if (dataStream.WorkdayLink) {
-    let data = dataStream.WorkdayLink.split(" ");
-    let url = "";
-    
-    for (let i = 0; i < data.length; i++) {
-      if( data[i].startsWith("https://www.myworkday.com")){
-        url = data[i];
-        return url;
-      }
-      else if (data[i].startsWith("mailto")) {
-        url = data[i];
-      }
-    }
-    if (url === "") {
-      url = "https://www.axiscapital.com/";
-    }
-    return url;
-    }
-    else {
-      return "https://www.axiscapital.com/";
-    }
+  if (props.file === "") {
+    docs = [
+      // { uri: require("../../things to do.docx") },
+      { uri: require("../../test.pdf") }
+      // {
+      //   uri:
+      //     "https://www.clickdimensions.com/links/TestPDFfile.pdf"
+      // }
+    ];
+  } else {
+    let temp = props.file;
+    console.log(temp);
+    docs = [{ uri: require(temp) }];
   }
+
+  const closeOnEsc = (e) => {
+    if ((e.charCode || e.keyCode) === 27) {
+      props.onClose();
+    }
+  };
+
+  useEffect(() => {
+    // setLoading(true);
+    document.body.addEventListener("keydown", closeOnEsc);
+    return function cleanup() {
+      document.body.removeEventListener("keydown", closeOnEsc);
+    };
+  });
+
+  if (!props.show) {
+    return null;
+  }
+  if (loading === true || !docs) {
+    return (
+      <div className="modal" onClick={props.onClose}>
+        {" "}
+        <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+          <h1>Content isn't finished loading</h1>{" "}
+          <p>Please close and try again </p>
+        </div>{" "}
+      </div>
+    );
+  }
+
+  return (
+    <div
+      className="modal"
+      onClick={props.onClose}
+      style={{ height: document.body.scrollHeight + 25 }}
+    >
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        <div className="modal-header">
+          <h4 className="modal-title">File Preview</h4>
+        </div>
+        <div className="modal-body">
+          <DocViewer
+            pluginRenderers={DocViewerRenderers}
+            documents={docs}
+            theme={{
+              primary: "#5296d8",
+              secondary: "#ffffff",
+              tertiary: "#5296d899",
+              text_primary: "#ffffff",
+              text_secondary: "#5296d8",
+              text_tertiary: "#00000099",
+              disableThemeScrollbar: false
+            }}
+            config={{
+              header: {
+                disableHeader: false,
+                disableFileName: true,
+                retainURLParams: true
+              }
+            }}
+          />
+        </div>
+        <div className="modal-footer">
+          <button onClick={props.onClose} className="exitButton">
+            X
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Modal;
